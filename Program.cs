04b8508e7dc13b49b52a8e9ef3bd702ecbcb4265
Volodymyr Shuran <vshuran@bizico.com>
2019-02-22 13:29:44 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TBot
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+
+        static void Main()
+        {
+            botClient = new TelegramBotClient("709032790:AAHF0ekIY51LSK58UCEKQvwmNtE73ATzTJc");
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text == "/start")
+            {
+                Console.WriteLine($"Satrt messaging in chat {e.Message.Chat.Id}.");
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "Ну че, драсте"
+                );
+            }
+            else if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                  chatId: e.Message.Chat,
+                  text: "You said:\n" + e.Message.Text
+                );
+            }
+            
+        }
+    }
+    
+}
